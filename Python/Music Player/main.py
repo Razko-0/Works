418cbdbfec4ac906a35dcb1b02b8d639308863f8
Razko-0 <razko.off@protonmail.com>
2026-09-13 18:44:56 +0200

    Add files via upload

---
+++ Python/Music Player/main.py
@@ -0,0 +1,83 @@
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import pygame
+
+def play_music(folder, music_name):
+
+    file_path = os.path.join(folder, music_name)
+    if not os.path.exists(file_path):
+        print("File not found.")
+        return
+
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+    print(f"Now playing: {music_name}")
+    print("Commands: [P]ause, [R]esume, [S]top")
+
+    while True:
+        command = input("> ").upper()
+
+        if command == "P":
+            pygame.mixer.music.pause()
+            print("Paused")
+        elif command == "R":
+            pygame.mixer.music.unpause()
+            print("Resumed")
+        elif command == "S":
+            pygame.mixer.music.stop()
+            print("Stopped")
+            return
+        else:
+            print("Invalid Command.")
+
+
+def main():
+# Init
+    try:
+        pygame.mixer.init()
+    except pygame.error as e:
+        print("Audio initialization failed ! ", e)
+        return
+
+# Variables
+    folder = "Musique"
+    mp3_files = [file for file in os.listdir(folder) if file.endswith(".mp3")]
+
+# Erreurs
+    if not os.path.isdir(folder):
+        print(f"folder '{folder}' not found.")
+
+    if not mp3_files:
+        print("No music files found.")
+
+# Boucle
+    while True:
+        # Deco
+        print("\n----- MUSIC PLAYER -----")
+        # 2 args possible grace à enumerate; start permet de commencer à compter à 1
+        for index, song in enumerate(mp3_files, start=1):
+            print(f"{index}. {song}")
+
+        choice_input = input("\nEnter the music index to play ('Q' to quit):")
+
+    # Errors
+        if not choice_input.isdigit():
+            print("Please enter a valid number.")
+            continue
+
+        # upper permet de mettre le char en maj (donc 'q' et 'Q' sont acceptés)
+        if choice_input.upper() == "Q":
+            break
+
+    # Selection
+        # Match l'index du son
+        choice = int(choice_input) - 1
+        if 0 <= choice < len(mp3_files):
+            play_music(folder, mp3_files[choice])
+        else:
+            print("Music index out of range !")
+        
+
+if __name__ == "__main__":
+    main()
